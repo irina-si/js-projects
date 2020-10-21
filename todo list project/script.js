57db637bc37cb79d0b26adaf88fc60c2ceef2fb9
Irina Sidorenko <sidorenko.irina.d@gmail.com>
2020-10-21 14:45:06 +0300

    Editing function

--- todo list project/script.js
+++ todo list project/script.js
@@ -20,14 +20,24 @@ function createList(item) {
     if (item.isChecked) {
         return `<li class='list-element' todoId=${item.id}>
                 <label class="list-element-completed"><input type="checkbox" name="scales" checked>${item.value}</label>
-                <span id="x">X</span>
+                <div class="controls"> 
+                <span class="button__edit"></span>
+                <span class="button__delete"></span>
+                </div>
                 </li>`;
         } else {
             return `<li class='list-element' todoId=${item.id}>
                     <label><input type="checkbox" name="scales">${item.value}</label>
-                    <span id="x">X</span>
+                    <div class="controls">            
+                    <span class="button__edit"></span>
+                    <span class="button__delete"></span>
+                    </div>
                     </li>`;
         }
+}
+
+function editingElement (item) {
+    return ;
 }
 
 function addItem(event) {
@@ -71,7 +81,6 @@ function checkItem(event) {
 }
 
 function deleteItem(event) {
-    if (event.target.id != "x") return;
     let currentId = event.target.closest("li").getAttribute("todoId");
     const data = JSON.parse(localStorage.getItem('items'));
     for (i = 0; i < data.length; i++) {
@@ -136,6 +145,51 @@ function showAllItems(event) {
     });
 }
 
+function editItem(event) {
+    event.preventDefault(); 
+    
+    let li = event.target.closest("li");
+    // li.tabIndex = "1";
+    li.insertAdjacentHTML("afterbegin", 
+    `<textarea id='edit' style="display:none" wrap="off">
+     ${li.querySelector("label").textContent}</textarea>`);
+
+    let textarea = document.getElementById("edit");
+    console.log(textarea);
+    focusIn();  
+
+    textarea.addEventListener("focusout", focusOut);
+    textarea.addEventListener("keydown", ()=> {
+      if (event.code == "Enter") focusOut();
+      })
+
+    function focusIn (event) {
+        textarea.style.display = 'block';
+        textarea.focus();
+        console.log(li.querySelector("label"));
+        li.querySelector("label").outerHTML = '';   
+    }
+  
+    function focusOut () {
+        li.insertAdjacentHTML("afterbegin", 
+        `<label><input type="checkbox" name="scales">${textarea.value}</label>`);
+        textarea.remove();
+        saveNewItem();
+    }
+    
+    function saveNewItem() {
+    let currentId = li.getAttribute("todoId");
+    const data = JSON.parse(localStorage.getItem('items'));
+    data.forEach((item) => {
+        if (item.id == currentId) {
+            item.value = li.querySelector("label").textContent;
+        }
+    })
+    localStorage.setItem('items', JSON.stringify(data));
+}
+    
+}
+
 const list = document.querySelector(".list");
 const addItemForm = document.getElementById("addItemForm");
 const completedFilterButton = document.getElementById("completed");
@@ -146,13 +200,16 @@ checkLocalStorage();
 getLocalStorageList();
 
 list.onchange = checkItem;
-addItemForm.onsubmit = addItem;
-list.onclick = deleteItem;
+addItemForm.onsubmit = addItem;  
 completedFilterButton.onclick = getCompletedItems;
 incompletedFilterButton.onclick = getIncompletedItems;
 showAllButton.onclick = showAllItems;
 
-
-
-
-
+list.addEventListener("click", function(event) {
+    console.log(event.target.classList);
+    console.dir(event.target);
+    if (event.target.classList.contains("button__edit")) {
+        editItem(event);
+    } else if (event.target.classList.contains("button__delete"))
+        deleteItem(event);
+})
